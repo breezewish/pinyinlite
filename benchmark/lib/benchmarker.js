17fc71fe09a91509bfc521ccf3003f8c0e84a585
Breezewish <me@breeswish.org>
2016-02-06 13:14:50 +0800

    improve benchmark

--- benchmark/lib/benchmarker.js
+++ benchmark/lib/benchmarker.js
@@ -14,16 +14,19 @@ Benchmarker.prototype.reset = function () {
 };
 
 Benchmarker.prototype.delta = function () {
-  var current = this.snap();
+  const current = this.snap();
   return {
     time: current.time - this.initial.time,
     memory: current.memory - this.initial.memory,
   };
 };
 
-Benchmarker.prototype.printDelta = function (str) {
-  var delta = this.delta();
-  console.log('%s: +%s KB, +%s ms', str, Math.floor(delta.memory / 1000), delta.time);
+Benchmarker.prototype.recordDelta = function (name) {
+  const delta = this.delta();
+  process.send({
+    name: name,
+    data: delta,
+  });
 };
 
 module.exports = Benchmarker;
---
+++ benchmark/run.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const async = require('async');
+const cp = require('child_process');
+const testsAll = 100;
+
+function runSingleCase(caseName, callback) {
+  let results = {};
+  async.timesSeries(testsAll, (i, next) => {
+    const child = cp.fork(`${__dirname}/case/${caseName}.js`);
+    child.on('message', (msg) => {
+      if (typeof results[msg.name] === 'undefined') {
+        results[msg.name] = {
+          time: 0,
+          memory: 0,
+        };
+      }
+      results[msg.name].time += msg.data.time;
+      results[msg.name].memory += msg.data.memory;
+    });
+    child.on('exit', () => {
+      next();
+    });
+  }, () => {
+    for (var key in results) {
+      results[key].time /= testsAll;
+      results[key].memory /= testsAll;
+    }
+    callback(null, results);
+  });
+}
+
+runSingleCase('1', function (err, results) {
+  console.log(results);
+});
